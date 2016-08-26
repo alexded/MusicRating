1c93825ae1ca55a3ca9364a9a580833d7206d43c
Alex DED <alexded@yahoo.com>
2016-08-26 19:07:20 +0300

    Some test data

--- src/main/java/adg/musicrating/Composition.java
+++ src/main/java/adg/musicrating/Composition.java
@@ -1,5 +1,24 @@
 package adg.musicrating;
 
-public interface Composition {
+import java.time.Duration;
+
+public class Composition {
+
+	private String name;
+	private int position;
+	private Duration duration;
+	private Rating rating;
+
+	public void setName( String name ) { this.name = name; }
+	public String getName() { return name; }
+
+	public void setPosition( int position ) { this.position = position; }
+	public int getPosition() { return position; }
+
+	public void setDuration( Duration duration ) { 	this.duration = duration; }
+	public Duration getDuration() { return duration; }
+
+	public void setRating( Rating rating ) { this.rating = rating; }
+	public Rating getRating() { return rating; }
 
 }
--- src/main/java/adg/musicrating/data/lindseystirling/BraveEnough.java
+++ src/main/java/adg/musicrating/data/lindseystirling/BraveEnough.java
@@ -1,5 +1,6 @@
 package adg.musicrating.data.lindseystirling;
 
+import java.time.Duration;
 import java.util.List;
 
 import adg.musicrating.Album;
@@ -14,6 +15,28 @@ public class BraveEnough implements Album {
 	private int year = 2016;
 	private List<Composition> compositions;
 	private Rating rating;
+	
+	public BraveEnough() {
+		String[] tracks = new String[] { 
+			"01 | Lost Girls | 04:35 | ?/1", 
+			"02 | Brave Enough ( feat. Christina Perri | 04:23 | ?/1",
+			"03 | The Arena | 03:52 | ?/1",
+			"04 | The Phoenix | 04:04 | ?/1",
+			"05 | Where Do We Go ( feat. Carah Faye ) | 04:15 | ?/1",
+			"06 | Those Days ( feat. Dan + Shay ) | 03:46 | ?/1",
+			"07 | Prism | 03:32 | ?/1",
+			"08 | Hold My Heart ( feat. ZZ Ward ) | 03:29 | ?/1",
+			"09 | Mirage ( feat. Raja Kumari ) | 04:22 | ?/1",
+			"10 | Don't Let This Feeling Fade ( feat. Rivers Cuomo & Lecrae ) | 03:36 | ?/1",
+			"11 | First Light | 03:23 | ?/1",
+			" |  |  | ?/1"
+		};
+		Composition composition01 = new Composition();
+		composition01.setPosition( 1 );
+		composition01.setName( "Lost Girls" );
+		composition01.setDuration( Duration.parse( "PT4M35S" ));
+		composition01.setRating( new Rating() );
+	}
 	
 	public Artist getArtist() { return this.artist; }
 	public String getName() { return this.name; }
